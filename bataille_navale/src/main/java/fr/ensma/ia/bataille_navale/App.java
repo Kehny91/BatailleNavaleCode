eaab94be55c4362a911a17266859585e8563054e
Kehny91 <lexou_k@hotmail.com>
2020-01-21 13:31:36 +0100

    creation du projet maven

---
+++ bataille_navale/src/main/java/fr/ensma/ia/bataille_navale/App.java
@@ -0,0 +1,13 @@
+package fr.ensma.ia.bataille_navale;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

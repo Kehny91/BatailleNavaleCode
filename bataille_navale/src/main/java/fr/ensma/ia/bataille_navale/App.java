600cbf92bbd20386399a781ee4086059793ce57f
Kehny91 <lexou_k@hotmail.com>
2020-03-16 13:00:06 +0100

    Debut JavaFx

--- bataille_navale/src/main/java/fr/ensma/ia/bataille_navale/App.java
+++ bataille_navale/src/main/java/fr/ensma/ia/bataille_navale/App.java
@@ -1,13 +1,21 @@
 package fr.ensma.ia.bataille_navale;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.show();
+	}
 }
